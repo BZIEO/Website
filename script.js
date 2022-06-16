7beecedf5ff4a16db9203ff11f5067d62ef00874
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-06-15 22:37:19 -0500

    Added password box

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const enterButton = document.querySelector(".btn--clickme");
 
+const passWord = "122996";
+let active = 0;
+const unlock = function () {
+  document.querySelector(".btn--clickme").textContent = "SIKE GOTTEM";
+  active++;
+};
+
 enterButton.addEventListener("click", function () {
-  alert("Try again later..");
+  if (active === 0) {
+    let userEntry = prompt(`What's the password?`);
+    if (userEntry === passWord) {
+      alert("ACCESS GRANTED");
+      unlock();
+    } else {
+      alert("ACCESS DENIED");
+    }
+  }
 });
