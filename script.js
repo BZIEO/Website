4a37f8fa51bac044d7583472fa2e6d900db1c6ea
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-06-22 00:09:55 -0500

    Passcode

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const enterButton = document.querySelector(".btn--clickme");
+const box = document.querySelector(".center--box");
+const birthDay = "122996";
+const passWord = "bzieo";
 
-const passWord = "122996";
 let active = 0;
-const unlock = function () {
+
+//Function if birthday is guessed
+const gottem = function () {
   document.querySelector(".btn--clickme").textContent = "SIKE GOTTEM";
   active++;
+};
+
+//Function if password guessed
+const unlock = function () {
+  enterButton.classList.add("hidden");
+  box.classList.add("entered-box");
+  location.replace("/main.html");
+};
+
+const exitScreen = function () {
+  let welcome = document.createElement("button");
+  let welcomeText = document.createTextNode("Exit");
+  welcome.appendChild(welcomeText);
+  welcome.classList.add("inverse");
+  box.appendChild(welcome);
 };
 
 enterButton.addEventListener("click", function () {
   if (active === 0) {
     let userEntry = prompt(`What's the password?`);
-    if (userEntry === passWord) {
+    if (userEntry === birthDay) {
       alert("ACCESS GRANTED 🟢 ");
+      gottem();
+    } else if (userEntry === passWord) {
       unlock();
     } else {
       alert("ACCESS DENIED 🛑 ");
